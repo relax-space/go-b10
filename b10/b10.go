c0f33b9f916fbe63d6f11bef34d461a2cac53665
xiaoxinmiao <xiaoxm_001@163.com>
2022-10-15 17:53:16 +0800

    v1.0.0

---
+++ b10/b10.go
@@ -0,0 +1,7 @@
+package b10
+
+import "github.com/relax-space/go-a10/a10"
+
+func Hello() string {
+	return a10.Hello()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/relax-space/go-b10
+
+go 1.19
+
+require github.com/relax-space/go-a10 v1.0.0
